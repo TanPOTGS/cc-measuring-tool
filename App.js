fb94bfe5d1c5030abf2941e03473313752159af8
TanPOTGS <paulroque13@gmail.com>
2021-04-27 10:54:42 -0500

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View 
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Calton Cases Measuring Tool</Text>
+      <Button
+        title="Start!"
+        onPress={() => navigation.navigate('Instrument Selection')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function InstrumentSelectionScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Please select an instrument type...</Text>
+      <Button
+        title="Acoustic"
+        onPress={() => navigation.navigate('Acoustic')}
+      />
+    </View>
+  );
+}
+
+function AcousticScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Start calibration!</Text>
+      <Button
+        title="Start"
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="Instrument Selection"
+          component={InstrumentSelectionScreen}
+          options={{ title: 'Instrument Selection' }}
+        />
+        <Stack.Screen
+          name="Acoustic"
+          component={AcousticScreen}
+          options={{ title: 'Acoustic' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
